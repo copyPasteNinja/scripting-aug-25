3a228f243547e6ad8cec0798c5c4edd69cbd834a
Sun <orionvantix@gmail.com>
2025-10-02 19:54:40 -0500

    adding hello script resolved conflict

--- users/script.sh
+++ users/script.sh
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 #!/bin/bash 
 
-### Script of conflics"
-
 echo "Hello from Abdul"
-=======
-## Script of conflicts
-
 echo "Hello from Sun"
->>>>>>> c439e52 (adding hello script)
+echo "Hello from Fatima"
+echo "Hello from Abdul"
